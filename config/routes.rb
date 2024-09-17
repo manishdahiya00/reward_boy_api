9987c2c8b74cdede59e7edbd8c1d72160c752bf3
Manish Dahiya <manishdahiya0944@gmail.com>
2024-09-17 15:35:32 +0530

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
+  root "main#index"
   mount API::Base => "/"
   get "/admin" => "admin/login#new"
   post "/admin" => "admin/login#login"
